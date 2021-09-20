9f3c8e48ef3fd2630fa287b040a98ea6725d54db
Adis Sertovic <adis.sertovic@mss.ba>
2021-09-20 12:32:59 +0200

    Storage repo

---
+++ StorageApplication/People.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageApplication
+{
+    public static class People
+    {
+
+
+        public static List<Person> DefaultList { get; set; } = new List<Person>();
+        public static string AddPerson(Person person)
+        {
+            DefaultList.Add(person);
+            return "Person Successfuly added";
+        }
+        public static Person GetPersonByName(string name)
+        {
+            return GetPeople().Select(x => x).Where(x => x.Name == "name").FirstOrDefault();
+        }
+        public static List<Person> PeopleList()
+        {
+
+            List<Person> list = new List<Person>();
+            list.AddRange(GetPeople());
+            list.AddRange(DefaultList);
+            return list;
+        }
+
+        public static List<Person> GetPeople()
+        {
+            List<Person> list = new List<Person>();
+            list.Add(new Person("Adis", "Sertovic", DateTime.Parse("1985-09-10"), "backend"));
+            list.Add(new Person("Emir", "Pajic", DateTime.Parse("1996-13-10"), "full stack"));
+            list.Add(new Person("Mirza", "Krzic", DateTime.Parse("1985-09-10"), "fontend"));
+            list.Add(new Person("Armin", "Comic", DateTime.Parse("1985-09-10"), "full stack system master"));
+            list.Add(new Person("Miralem", "Avdic", DateTime.Parse("1985-09-10"), "fontend"));
+            return list;
+        }
+    }
+}
---
+++ StorageApplication/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageApplication
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Agre { get; set; }
+        public string ProgrammingOrientation { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(string name, string lastName, DateTime agre, string programmingOrientation)
+        {
+            Name = name;
+            LastName = lastName;
+            Agre = agre;
+            ProgrammingOrientation = programmingOrientation;
+        }
+    }
+}
